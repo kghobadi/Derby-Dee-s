389f4b1a66dfcb6d7580b80ff59df507f08db854
Unknown <ksg304@nyu.edu>
2019-04-18 23:51:30 -0400

    vehicle script

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    public VehicleType vehicleType;
+
+    public enum VehicleType
+    {
+        BASIC, FAST, KATAMARI,
+    }
+
+    public void UseVehicle()
+    {
+        if(vehicleType == VehicleType.BASIC)
+        {
+            //GetComponent<Basic>().UseVehicle();
+        }
+
+        if (vehicleType == VehicleType.FAST)
+        {
+            //GetComponent<Fast>().UseVehicle();
+        }
+
+        if (vehicleType == VehicleType.KATAMARI)
+        {
+            //GetComponent<Katamari>().UseVehicle();
+        }
+    }
+}
